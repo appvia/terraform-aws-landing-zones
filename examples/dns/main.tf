eb2dbdd1f01b2c6b4212ea5c07dd61144c7b3daf
Rohith Jayawardene <gambol99@gmail.com>
2024-06-12 17:40:26 +0100

    chore: ignore these for now

--- examples/dns/main.tf
+++ examples/dns/main.tf
@@ -4,10 +4,12 @@
 # to build your own root module that invokes this module
 #####################################################################################
 
-#trivy:ignore:AVD-DS-0002
-#trivy:ignore:AVD-DS-0013
-#trivy:ignore:AVD-DS-0015
-#trivy:ignore:AVD-DS-0026
+#tfsec:ignore:AVD-DS-0002
+#tfsec:ignore:AVD-DS-0013
+#tfsec:ignore:AVD-DS-0015
+#tfsec:ignore:AVD-DS-0026
+#tfsec:ignore:AVD-AWS-0067
+#tfsec:ignore:AVD-AWS-0057
 module "app1" {
   source = "../../"
 
