0ed3719ea1fdd6759f4edb1cce21dcdef96f2d48
Rohith Jayawardene <gambol99@gmail.com>
2024-10-10 21:29:03 +0100

    chore: adding more roles mappings

--- settings.general.tf
+++ settings.general.tf
@@ -15,6 +15,11 @@ locals {
   ## The permitted permission sets that can be assigned to the account, and their corresponding permission set 
   ## in identity center; unless the permissionset is mentioned here, it cannot be assigned to the account 
   sso_permitted_permission_sets = {
+    "administrator"     = "Administrator"
+    "backup_admin"      = "BackupAdmin"
+    "backup_operator"   = "BackupOperator"
+    "billing_admin"     = "BillingAdmin"
+    "billing_viewer"    = "BillingViewer"
     "devops_engineer"   = "DevOpsEngineer"
     "finops_engineer"   = "FinOpsEngineer"
     "network_engineer"  = "NetworkEngineer"
