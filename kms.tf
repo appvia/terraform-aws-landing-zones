7a64c1caa00797ae7967753ea935354b8701ba95
Rohith Jayawardene <gambol99@gmail.com>
2024-12-10 21:12:45 +0000

    fix: wrapping the statement in a try()

--- kms.tf
+++ kms.tf
@@ -3,10 +3,10 @@ locals {
   ## Indicates if we should provision a default kms key for the account (per region)
   enable_kms_key = var.kms_key.enable
 
-  ## Should we create a default kms key administrator role for the account 
+  ## Should we create a default kms key administrator role for the account
   enable_kms_key_administrator = var.kms_administrator.enable
 
-  ## List of roles or accounts whom can assume the kms key administrator role 
+  ## List of roles or accounts whom can assume the kms key administrator role
   kms_key_administrator_roles = concat(
     var.kms_administrator.enable_account_root ? [format("arn:aws:iam::%s:root", local.account_id)] : [],
     var.kms_key.key_administrators,
@@ -16,17 +16,17 @@ locals {
   ## Is the name of the key administrator iam role within the account
   kms_key_administrator_role_name = var.kms_administrator.name
 
-  ## The description added to the kms key administrator role 
+  ## The description added to the kms key administrator role
   kms_key_administrator_role_description = var.kms_administrator.description != null ? var.kms_administrator.description : "Provides access to administer the KMS keys for the account"
 
-  ## This will have the create_kms_key_administrator role arn if required, else it will be an empty list 
-  kms_key_administrator_role_arn = local.enable_kms_key_administrator ? module.kms_key_administrator[0].iam_role_arn : null
+  ## This will have the create_kms_key_administrator role arn if required, else it will be an empty list
+  kms_key_administrator_role_arn = local.enable_kms_key_administrator ? try(module.kms_key_administrator[0].iam_role_arn, null) : null
 
   ## A list of roles who should be able to administer the kms key
   kms_key_owners = concat(var.kms_key.key_administrators, [local.kms_key_administrator_role_arn])
 }
 
-## Provision the key administrator role for the account if required 
+## Provision the key administrator role for the account if required
 module "kms_key_administrator" {
   count   = local.enable_kms_key_administrator && local.home_region ? 1 : 0
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
