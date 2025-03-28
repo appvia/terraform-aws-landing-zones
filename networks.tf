d657e2bcd534ffdda5412ab6538d1b2cc5e534c0
Rohith Jayawardene <gambol99@gmail.com>
2025-03-28 06:38:04 +0000

    chore(deps): bumping the version of the network module (#79)

--- networks.tf
+++ networks.tf
@@ -1,6 +1,6 @@
 
 locals {
-  ## A map of transit gateway asssociations which should be made 
+  ## A map of transit gateway asssociations which should be made
   transit_gateway_associations = {
     for k, v in var.networks : k => v.transit_gateway.gateway_route_table_id if v.vpc.enable_transit_gateway && v.transit_gateway.gateway_id != null && v.transit_gateway.gateway_route_table_id != null
   }
@@ -10,22 +10,20 @@ locals {
 module "networks" {
   for_each = var.networks
   source   = "appvia/network/aws"
-  version  = "0.3.4"
+  version  = "0.4.0"
 
-  additional_subnets                     = { for k, v in each.value.subnets : k => v if !contains(["public", "private"], k) }
   availability_zones                     = each.value.vpc.availability_zones
   enable_default_route_table_association = each.value.vpc.enable_default_route_table_association
   enable_default_route_table_propagation = each.value.vpc.enable_default_route_table_propagation
-  enable_ipam                            = each.value.vpc.ipam_pool_name != null ? true : false
   enable_private_endpoints               = each.value.vpc.enable_private_endpoints
   enable_route53_resolver_rules          = each.value.vpc.enable_shared_endpoints
-  enable_transit_gateway                 = each.value.vpc.enable_transit_gateway
   enable_transit_gateway_appliance_mode  = each.value.vpc.enable_transit_gateway_appliance_mode
   ipam_pool_id                           = each.value.vpc.ipam_pool_name != null ? local.ipam_pools_by_name[each.value.vpc.ipam_pool_name] : null
   name                                   = each.key
   nat_gateway_mode                       = each.value.vpc.nat_gateway_mode
   private_subnet_netmask                 = coalesce(try(each.value.subnets["private"].netmask, null), 0)
   public_subnet_netmask                  = coalesce(try(each.value.subnets["public"].netmask, null), 0)
+  subnets                                = { for k, v in each.value.subnets : k => v if !contains(["public", "private"], k) }
   tags                                   = merge(local.tags, each.value.tags)
   transit_gateway_id                     = each.value.vpc.enable_transit_gateway ? each.value.transit_gateway.gateway_id : null
   transit_gateway_routes                 = each.value.vpc.enable_transit_gateway ? each.value.transit_gateway_routes : {}
@@ -37,8 +35,8 @@ module "networks" {
   }
 }
 
-## For each of the networks, attach them to the appropriate transit gateway routing 
-## table if required 
+## For each of the networks, attach them to the appropriate transit gateway routing
+## table if required
 resource "aws_ec2_transit_gateway_route_table_association" "asssociation" {
   for_each = local.transit_gateway_associations
 
