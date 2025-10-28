cb41ccdb5b4d374536363b2d6d0eeb3cdf514d4b
Rohith Jayawardene <gambol99@gmail.com>
2025-10-28 10:35:09 +0000

    fix: resolved the ssm value to Enable or Disable

--- ssm.tf
+++ ssm.tf
@@ -5,7 +5,7 @@
 ## Configure the SSM service setting to block public sharing
 resource "aws_ssm_service_setting" "ssm_service_setting_block_public_sharing" {
   setting_id    = "/ssm/documents/console/public-sharing-permission"
-  setting_value = var.ssm.enable_block_public_sharing ? "Enabled" : "Disabled"
+  setting_value = var.ssm.enable_block_public_sharing ? "Enable" : "Disable"
 
   provider = aws.tenant
 }
