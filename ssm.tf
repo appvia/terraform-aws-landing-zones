169ed8d48e767df99ed7606e7e8d4d968b86e675
Rohith Jayawardene <gambol99@gmail.com>
2025-12-03 09:17:58 +0000

    chore: added an region to ensure we block in the correct region

--- ssm.tf
+++ ssm.tf
@@ -4,6 +4,7 @@
 
 ## Configure the SSM service setting to block public sharing
 resource "aws_ssm_service_setting" "ssm_service_setting_block_public_sharing" {
+  region        = local.region
   setting_id    = "/ssm/documents/console/public-sharing-permission"
   setting_value = var.ssm.enable_block_public_sharing ? "Enable" : "Disable"
 
