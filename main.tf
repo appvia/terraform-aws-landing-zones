09a0356e8612594b85403f1663cf032c1a3158e2
Rohith Jayawardene <gambol99@gmail.com>
2025-12-04 16:20:42 +0000

    chore: bumped to the latest version

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "tagging" {
 #trivy:ignore:AVD-DS-0026
 module "notifications" {
   source  = "appvia/notify/aws"
-  version = "0.1.0"
+  version = "0.1.1"
 
   allowed_aws_services = [
     "budgets.amazonaws.com",
