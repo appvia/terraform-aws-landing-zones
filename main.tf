5f212dd6a9ca7a03e25430a61b129736c7fd0441
Rohith Jayawardene <gambol99@gmail.com>
2026-05-02 21:50:07 +0100

    chore: bumped to the latest version of the notify module

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "tagging" {
 #trivy:ignore:AVD-DS-0026
 module "notifications" {
   source  = "appvia/notify/aws"
-  version = "0.1.3"
+  version = "0.1.4"
 
   allowed_aws_services = [
     "budgets.amazonaws.com",
