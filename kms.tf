appvia/terraform-aws-landing-zones

commit 748ac95cf51c17cc7f37e0725352087c808ec85e

feat: resolving elements highlighted in the security review

kms.tf
@@ -9,12 +9,35 @@ locals {
   enable_kms_key_administrator = var.kms_administrator.enable
 
   ## Is a list of roles and or accounts whom should have a assume trust into the kms
-  ## key administrator role.
+  ## key administrator role. Note this is deliberately distinct from
+  ## var.kms_key.key_administrators, which controls who is named in the key policy - being
+  ## a principal in the key policy does not imply permission to assume the administrator role.
   kms_key_administrator_roles = concat(
     var.kms_administrator.enable_account_root ? [format("arn:aws:iam::%s:root", local.account_id)] : [],
-    var.kms_key.key_administrators,
+    var.kms_administrator.assume_roles,
     [for x in var.kms_administrator.assume_accounts : format("arn:aws:iam::%s:root", x)],
   )
+
+  ## The administrative actions permitted to the kms key administrator role. Note this list
+  ## deliberately excludes kms:PutKeyPolicy (and the kms:Put* wildcard which would cover it),
+  ## so that a key administrator cannot rewrite the key policy to grant themselves the ability
+  ## to perform cryptographic operations with the key.
+  kms_key_administrator_actions = [
+    "kms:Create*",
+    "kms:Delete*",
+    "kms:Describe*",
+    "kms:Disable*",
+    "kms:Enable*",
+    "kms:Get*",
+    "kms:List*",
+    "kms:Revoke*",
+    "kms:ScheduleKeyDeletion",
+    "kms:TagResource",
+    "kms:UntagResource",
+    "kms:UpdateAlias",
+    "kms:UpdateKeyDescription",
+    "kms:UpdatePrimaryRegion",
+  ]
 
   ## If we are using the kms key administrator role, this WOULD be the ARN for the provisioned
   kms_key_administrator_role_arn = format("arn:aws:iam::%s:role/%s", local.account_id, var.kms_administrator.name)
@@ -37,8 +60,35 @@ locals {
     )
   )
 
-  ## The arns who wil be users for the kms key 
+  ## The arns who wil be users for the kms key
   kms_key_users = try(var.kms_key.key_users, null)
+
+  ## The organization guardrail statement. This is always appended to the key policy, even when
+  ## the tenant supplies their own key_statements, so the guardrail cannot be removed by input.
+  kms_key_organization_statement = {
+    sid    = "DenyAccessOutsideOrg"
+    effect = "Deny"
+    principals = [
+      {
+        type        = "AWS"
+        identifiers = ["*"]
+      }
+    ]
+    actions   = ["kms:*"]
+    resources = ["*"]
+    condition = [
+      {
+        test     = "StringNotEquals"
+        variable = "aws:PrincipalOrgID"
+        values   = [local.organization_id]
+      },
+      {
+        test     = "Bool"
+        variable = "aws:PrincipalIsAWSService"
+        values   = ["false"]
+      }
+    ]
+  }
 }
 
 ## Provision the key administrator role for the account if required
@@ -92,9 +142,9 @@ module "kms_key_administrator" {
   # Permissions for the key administrator role
   inline_policy_permissions = {
     "kms" : {
-      sid       = "AllowKMSKeyActions"
-      effect    = "Allow"
-      actions   = ["kms:*"]
+      sid     = "AllowKMSKeyActions"
+      effect  = "Allow"
+      actions = local.kms_key_administrator_actions
       resources = [
         "arn:aws:kms:${local.region}:${local.account_id}:key/*"
       ]
@@ -120,104 +170,71 @@ module "kms_key" {
   multi_region            = false
   tags                    = merge(local.tags, { "Name" = var.kms_key.key_alias })
 
-  key_statements = var.kms_key.key_statements != null ? var.kms_key.key_statements : compact([
-    local.enable_kms_key_administrator ? {
-      "Sid": "AllowKeyAdministration",
-      effect = "Allow"
-      principals = [
-        {
-          type = "AWS"
-          identifiers = [local.kms_key_administrator_role_arn]
-        }
-      ]
-      actions = [
-        "kms:Create*", 
-        "kms:Delete*", 
-        "kms:Describe*", 
-        "kms:Disable*",
-        "kms:Enable*", 
-        "kms:Get*", 
-        "kms:List*",
-        "kms:Put*", 
-        "kms:Revoke*", 
-        "kms:ScheduleKeyDeletion", 
-        "kms:TagResource", 
-        "kms:UntagResource",
-        "kms:Update*", 
-      ]
-      resources = ["*"]
-    } : null,
-    {
-      sid = "AllowUseViaAWSServices"
-      effect = "Allow"
-      actions = [
-        "kms:Decrypt", 
-        "kms:DescribeKey",
-        "kms:Encrypt", 
-        "kms:GenerateDataKey*", 
-        "kms:ReEncrypt*",
-      ]
-      resources = ["*"]
-      condition = [
-        {
-          test     = "StringEquals"
-          variable = "kms:CallerAccount"
-          values = [local.account_id]
-        },
-        {
-          test     = "StringLike"
-          variable = "kms:ViaService"
-          values = ["*.${local.region}.amazonaws.com"]
-        }
-      ]
-    },
-    {
-      sid = "AllowGrantsForAWSResources"
-      effect = "Allow"
-      actions = [
-        "kms:CreateGrant", 
-        "kms:ListGrants", 
-        "kms:RevokeGrant"
-      ]
-      resources = ["*"]
-      condition = [
-        {
-          test     = "StringEquals"
-          variable = "kms:CallerAccount"
-          values = [local.account_id]
-        },
-        {
-          test     = "Bool"
-          variable = "kms:GrantIsForAWSResource"
-          values = ["true"]
-        }
-      ]
-    },
-    {
-      sid = "DenyAccessOutsideOrg"
-      effect = "Deny"
-      principals = [
-        {
-          type = "AWS"
-          identifiers = ["*"]
-        }
-      ]
-      actions = ["kms:*"]
-      resources = ["*"]
-      condition = [
-        {
-          test     = "StringNotEquals"
-          variable = "aws:PrincipalOrgID"
-          values = [local.organization_id]
-        },
-        {
-          test     = "Bool"
-          variable = "aws:PrincipalIsAWSService"
-          values = ["false"]
-        }
-      ]
-    }
-  ])
+  ## The organization guardrail is appended unconditionally - a tenant supplying key_statements
+  ## replaces the default statements, but can never remove the deny outside the organization.
+  key_statements = concat(
+    var.kms_key.key_statements != null ? var.kms_key.key_statements : compact([
+      local.enable_kms_key_administrator ? {
+        sid    = "AllowKeyAdministration"
+        effect = "Allow"
+        principals = [
+          {
+            type        = "AWS"
+            identifiers = [local.kms_key_administrator_role_arn]
+          }
+        ]
+        actions   = local.kms_key_administrator_actions
+        resources = ["*"]
+      } : null,
+      {
+        sid    = "AllowUseViaAWSServices"
+        effect = "Allow"
+        actions = [
+          "kms:Decrypt",
+          "kms:DescribeKey",
+          "kms:Encrypt",
+          "kms:GenerateDataKey*",
+          "kms:ReEncrypt*",
+        ]
+        resources = ["*"]
+        condition = [
+          {
+            test     = "StringEquals"
+            variable = "kms:CallerAccount"
+            values   = [local.account_id]
+          },
+          {
+            test     = "StringLike"
+            variable = "kms:ViaService"
+            values   = ["*.${local.region}.amazonaws.com"]
+          }
+        ]
+      },
+      {
+        sid    = "AllowGrantsForAWSResources"
+        effect = "Allow"
+        actions = [
+          "kms:CreateGrant",
+          "kms:ListGrants",
+          "kms:RevokeGrant"
+        ]
+        resources = ["*"]
+        condition = [
+          {
+            test     = "StringEquals"
+            variable = "kms:CallerAccount"
+            values   = [local.account_id]
+          },
+          {
+            test     = "Bool"
+            variable = "kms:GrantIsForAWSResource"
+            values   = ["true"]
+          }
+        ]
+      },
+    ]),
+    [local.kms_key_organization_statement],
+  )
 
   depends_on = [
     module.kms_key_administrator,

variables.tf
@@ -162,6 +162,17 @@ variable "cloudwatch" {
     observability_sink    = null
     observability_source  = null
   }
+
+  ## An enabled sink with neither identifiers nor an organization_id would permit any AWS
+  ## account to link into it, so we make that combination unrepresentable.
+  validation {
+    condition = (
+      try(var.cloudwatch.observability_sink.enable, false) == false ||
+      try(length(var.cloudwatch.observability_sink.identifiers), 0) > 0 ||
+      try(length(var.cloudwatch.observability_sink.organization_id), 0) > 0
+    )
+    error_message = "When the observability sink is enabled you must set at least one of identifiers or organization_id, otherwise any AWS account can link into the sink"
+  }
 }
 
 variable "cost_anomaly_detection" {
@@ -210,6 +221,8 @@ variable "dns" {
   type = map(object({
     # A comment associated with the DNS zone
     comment = optional(string, "Managed by zone created by terraform")
+    # A flag indicating if the zone can be destroyed while it still contains records
+    force_destroy = optional(bool, true)
     # A list of network names to associate with the DNS zone
     network = string
     # A flag indicating if the DNS zone is private or public
@@ -324,7 +337,7 @@ variable "iam_access_analyzer" {
   description = "The IAM access analyzer configuration to apply to the account"
   type = object({
     # A flag indicating if IAM access analyzer should be enabled
-    enable = optional(bool, false)
+    enable = optional(bool, true)
     # The name of the IAM access analyzer
     analyzer_name = optional(string, "lza-iam-access-analyzer")
     # The type of the IAM access analyzer
@@ -475,6 +488,8 @@ variable "iam_service_linked_roles" {
 variable "iam_users" {
   description = "A collection of IAM users to apply to the account"
   type = list(object({
+    # A flag indicating if the user can be destroyed while it still has attached resources
+    force_destroy = optional(bool, true)
     # The name of the IAM user
     name = optional(string, null)
     # The name prefix of the IAM user
@@ -842,6 +857,7 @@ variable "networks" {
 
 variable "notifications" {
   description = "Configuration for the notifications to the owner of the account"
+  sensitive   = true
   type = object({
     email = optional(object({
       # A list of email addresses to send notifications to

.trivyignore
@@ -2,7 +2,6 @@ AVD-DS-0002  # (Dockerfile) Least Privilege User (HIGH) - https://avd.aquasec.co
 AVD-DS-0013  # (Dockerfile) Use Workdir Over Cd (MEDIUM) - https://avd.aquasec.com/misconfig/dockerfile/general/avd-ds-0013/
 AVD-DS-0015  # (Dockerfile) Use COPY instead of ADD (MEDIUM) - https://avd.aquasec.com/misconfig/dockerfile/general/avd-ds-0015/
 AVD-DS-0026  # (Dockerfile) Add HEALTHCHECK instruction in your Dockerfile (LOW) - https://avd.aquasec.com/misconfig/dockerfile/general/avd-ds-0013/
-AVD-AWS-0057 
 AVD-AWS-0067
 AVD-AWS-0095 # https://avd.aquasec.com/misconfig/aws/sns/avd-aws-0095/
 AVD-DS-0001  # https://avd.aquasec.com/misconfig/dockerfile/general/avd-ds-0001/

dns.tf
@@ -21,7 +21,7 @@ resource "aws_route53_zone" "zones" {
 
   name          = each.key
   comment       = try(each.value.comment, "Managed by zone created by terraform")
-  force_destroy = true
+  force_destroy = each.value.force_destroy
   tags          = merge(local.tags, { "Name" = each.key })
 
   dynamic "vpc" {

ebs.tf
@@ -22,9 +22,15 @@ locals {
 ## Additional IAM policy document for EBS encryption kms key
 data "aws_iam_policy_document" "ebs_encryption_key" {
   statement {
-    sid       = "AllowEC2"
-    effect    = "Allow"
-    actions   = ["kms:*"]
+    sid    = "AllowEC2"
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:DescribeKey",
+      "kms:Encrypt",
+      "kms:GenerateDataKey*",
+      "kms:ReEncrypt*",
+    ]
     resources = ["*"]
 
     principals {

iam.tf
@@ -44,7 +44,7 @@ module "iam_users" {
 
   create_access_key    = false
   create_login_profile = false
-  force_destroy        = true
+  force_destroy        = each.value.force_destroy
   name                 = each.value.name
   path                 = each.value.path
   permissions_boundary = each.value.permissions_boundary_name != null ? format("arn:aws:iam::%s:policy/%s", local.account_id, each.value.permissions_boundary_name) : ""
@@ -98,7 +98,7 @@ resource "aws_iam_account_password_policy" "iam_account_password_policy" {
 
 ## Configure the IAM Access Analyzer for the account
 resource "aws_accessanalyzer_analyzer" "iam_access_analyzer" {
-  count = var.iam_access_analyzer.enable ? 1 : 0
+  count = local.home_region && var.iam_access_analyzer.enable ? 1 : 0
 
   analyzer_name = var.iam_access_analyzer.analyzer_name
   tags          = merge(local.tags, { "Name" = var.iam_access_analyzer.analyzer_name })

oam.tf
@@ -4,7 +4,10 @@ locals {
   ## The observability sink configuration
   observability_sink = try(var.cloudwatch.observability_sink, {})
   ## Indicates if the observability sink should be enabled
-  enable_observability_sink = try(local.observability_sink.enable, false) && length(try(local.observability_sink.identifiers, [])) > 0
+  enable_observability_sink = try(local.observability_sink.enable, false)
+  ## The AWS principals permitted to link into the observability sink. The cloudwatch variable
+  ## validation guarantees this is non-empty whenever no organization_id has been configured.
+  observability_sink_identifiers = coalesce(try(local.observability_sink.identifiers, null), [])
   ## Indicates if cloudwatch cross-account observability should be enabled
   enable_observability_source = try(local.observability_source.enable, false) && try(local.observability_source.account_id, null) != null
   ## The account id for the cloudwatch cross-account observability
@@ -36,9 +39,11 @@ data "aws_iam_policy_document" "observability_sink_policy" {
     ]
     resources = ["*"]
 
+    ## Where identifiers have been configured we scope the policy to those principals, otherwise
+    ## we fall back to the organization, which the variable validation guarantees is set.
     principals {
       type        = "AWS"
-      identifiers = ["*"]
+      identifiers = length(local.observability_sink_identifiers) > 0 ? local.observability_sink_identifiers : ["*"]
     }
 
     dynamic "condition" {

commit c40c9259fd405bf9c38b03b9df4e23d23460631b

fix: unblock apply on sensitive for_each and compact() of policy objects
Two errors introduced by 748ac95 broke terraform apply in the pipeline.

var.notifications was marked sensitive, which tainted the email addresses
flowing into the notifications submodule where they are used as a for_each
key. Terraform rejects sensitive values as instance keys since they would
be exposed in state addresses and plan output. The marker is reverted;
var.infrastructure_repository stays sensitive as webhooks[].secret is a
genuine credential and outputs.tf already wraps it in nonsensitive().

The default kms key policy used compact() to drop a conditional null from
a list of statement objects, but compact() only accepts list(string).
Replaced with concat() over a conditional list, which also preserves the
differing statement shapes - the administration statement carries
principals and no condition, the others the reverse.

Co-Authored-By: Claude Opus 4.8 <noreply@anthropic.com>

kms.tf
@@ -179,8 +179,8 @@ module "kms_key" {
   ## The organization guardrail is appended unconditionally - a tenant supplying key_statements
   ## replaces the default statements, but can never remove the deny outside the organization.
   key_statements = concat(
-    var.kms_key.key_statements != null ? var.kms_key.key_statements : compact([
-      local.enable_kms_key_administrator ? {
+    var.kms_key.key_statements != null ? var.kms_key.key_statements : concat(
+      local.enable_kms_key_administrator ? [{
         sid    = "AllowKeyAdministration"
         effect = "Allow"
         principals = [
@@ -191,8 +191,8 @@ module "kms_key" {
         ]
         actions   = local.kms_key_administrator_actions
         resources = ["*"]
-      } : null,
-      {
+      }] : [],
+      [{
         sid    = "AllowUseViaAWSServices"
         effect = "Allow"
         actions = [
@@ -215,30 +215,30 @@ module "kms_key" {
             values   = ["*.${local.region}.amazonaws.com"]
           }
         ]
-      },
-      {
-        sid    = "AllowGrantsForAWSResources"
-        effect = "Allow"
-        actions = [
-          "kms:CreateGrant",
-          "kms:ListGrants",
-          "kms:RevokeGrant"
-        ]
-        resources = ["*"]
-        condition = [
-          {
-            test     = "StringEquals"
-            variable = "kms:CallerAccount"
-            values   = [local.account_id]
-          },
-          {
-            test     = "Bool"
-            variable = "kms:GrantIsForAWSResource"
-            values   = ["true"]
-          }
-        ]
-      },
-    ]),
+        },
+        {
+          sid    = "AllowGrantsForAWSResources"
+          effect = "Allow"
+          actions = [
+            "kms:CreateGrant",
+            "kms:ListGrants",
+            "kms:RevokeGrant"
+          ]
+          resources = ["*"]
+          condition = [
+            {
+              test     = "StringEquals"
+              variable = "kms:CallerAccount"
+              values   = [local.account_id]
+            },
+            {
+              test     = "Bool"
+              variable = "kms:GrantIsForAWSResource"
+              values   = ["true"]
+            }
+          ]
+      }],
+    ),
     [local.kms_key_organization_statement],
   )
 

variables.tf
@@ -867,7 +867,6 @@ variable "networks" {
 
 variable "notifications" {
   description = "Configuration for the notifications to the owner of the account"
-  sensitive   = true
   type = object({
     email = optional(object({
       # A list of email addresses to send notifications to